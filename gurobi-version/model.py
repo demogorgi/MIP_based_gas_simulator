beb2f2129253994f388a2b8e33e91689a7a16c9f
Uwe Gotzes <u.gotzes@gmail.com>
2020-07-25 23:03:23 +0200

    adjusted compressor model: gas=0 yields flow=0 now

--- gurobi-version/model.py
+++ gurobi-version/model.py
@@ -235,7 +235,11 @@ def simulate(agent_decisions,compressors,t,dt):
     m.addConstrs((var_non_pipe_Qo[cs] >= 0 for cs in co.compressors), name='compressor_eq_one')
     #
     #subto compressor_eq_two: forall <l,r> in CS:
-    m.addConstrs((var_non_pipe_Qo[cs] == compressor_DA[cs] * 3.6 * p_old(t,cs[0]) * phi_new(compressor_DA[cs],compressors[joiner(cs)]["phi_min"],compressors[joiner(cs)]["phi_max"],compressors[joiner(cs)]["pi_1"],compressors[joiner(cs)]["pi_2"],compressors[joiner(cs)]["L_min_pi"],compressors[joiner(cs)]["L_max_pi"],compressors[joiner(cs)]["L_min_phi"],compressors[joiner(cs)]["p_i_min"],compressors[joiner(cs)]["p_i_max"],compressors[joiner(cs)]["L_max_pi"],compressors[joiner(cs)]["eta"],get_agent_decision(agent_decisions["gas"]["CS"][joiner(cs)],t),p_old(t,cs[0]),p_old(t,cs[1])) for cs in co.compressors), name='compressor_eq_two')
+    for cs in co.compressors:
+        if get_agent_decision(agent_decisions["gas"]["CS"][joiner(cs)],t) == 0:
+            m.addConstr((var_non_pipe_Qo[cs] == 0), name='compressor_eq_two_a_' + str(cs))
+        else:
+            m.addConstr((var_non_pipe_Qo[cs] == compressor_DA[cs] * 3.6 * p_old(t,cs[0]) * phi_new(compressor_DA[cs],compressors[joiner(cs)]["phi_min"],compressors[joiner(cs)]["phi_max"],compressors[joiner(cs)]["pi_1"],compressors[joiner(cs)]["pi_2"],compressors[joiner(cs)]["L_min_pi"],compressors[joiner(cs)]["L_max_pi"],compressors[joiner(cs)]["L_min_phi"],compressors[joiner(cs)]["p_i_min"],compressors[joiner(cs)]["p_i_max"],compressors[joiner(cs)]["L_max_pi"],compressors[joiner(cs)]["eta"],get_agent_decision(agent_decisions["gas"]["CS"][joiner(cs)],t),p_old(t,cs[0]),p_old(t,cs[1]))), name='compressor_eq_two_b_' + str(cs))
     #
     #### Entrymodellierung ###
     #subto entry_flow_model:
